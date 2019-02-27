a075595fecc136470e7e812be04625e870b038cd
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2019-02-27 09:48:39 +0100

    Add project files.

---
+++ Test/ClubMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public enum Gender { Male, Female };
+    public class ClubMember : IComparable<ClubMember>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(ClubMember obj)
+        {
+            //ClubMember club = (ClubMember)(obj);
+            return this.FirstName.CompareTo(obj.FirstName);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {FirstName} {LastName} ({Gender}, {Age} years)";
+        }
+    }
+}
---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d73d135-0a17-45d5-a0c1-08b00cd185db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
